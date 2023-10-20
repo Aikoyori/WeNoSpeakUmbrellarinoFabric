4d219975577789dee2a6d772d6da766d90eed133
Lura Skye <l@veriny.tf>
2023-10-20 04:30:38 +0100

    1.20

--- src/main/java/xyz/aikoyori/wenospeakumbrellarino/Wenospeakumbrellarino.java
+++ src/main/java/xyz/aikoyori/wenospeakumbrellarino/Wenospeakumbrellarino.java
@@ -1,19 +1,25 @@
 package xyz.aikoyori.wenospeakumbrellarino;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.registry.Registry;
 import xyz.aikoyori.wenospeakumbrellarino.item.UmbrellaItem;
 
 public class Wenospeakumbrellarino implements ModInitializer {
 
     public static final String MOD_ID = "wenospeakumbrellarino";
-    public static final UmbrellaItem UMBRELLA_ITEM = new UmbrellaItem(new Item.Settings().group(ItemGroup.TOOLS));
+    public static final UmbrellaItem UMBRELLA_ITEM = new UmbrellaItem(new Item.Settings());
+
     @Override
     public void onInitialize() {
-        Registry.register(Registry.ITEM,new Identifier(MOD_ID,"umbrella"),UMBRELLA_ITEM);
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.TOOLS).register(content -> {
+            content.add(UMBRELLA_ITEM);
+        });
 
+        Registry.register(Registries.ITEM, new Identifier(MOD_ID, "umbrella"), UMBRELLA_ITEM);
     }
 }
